e2c8a099948b15b2e7b224f7abdc1417aa24b02c
gillerick <ogayogill95@gmail.com>
2021-01-13 23:35:21 +0300

    Added an alternative solution

--- Lists Problem.py
+++ Lists Problem.py
@@ -36,3 +36,36 @@ for c in C:
         A.pop(int(c[1]))
     elif "append" in c:
         A.append(c[1])
+
+
+"""
+Alternative solution:
+L = []
+N = int(input())
+while 1:
+    try:
+        inp = raw_input().strip()
+        while "  " in inp: inp.replace("  "," ")
+        command = map(str,inp.split())
+    except:
+        break
+    if len(command)>1: command[1]=int(command[1])
+    if len(command)>2: command[2]=int(command[2])
+        
+    if command[0]=="sort":
+        L.sort()
+    elif command[0]=="print":
+        print L
+    elif command[0]=="reverse":
+        #L.reverse()
+        L=L
+    elif command[0]=="pop":
+        L.pop()
+    elif command[0]=="insert":
+        L.insert(command[1],command[2])
+    elif command[0]=="append":
+        L.append(command[1])
+    elif command[0]=="remove":
+        L.remove(command[1])
+
+"""
