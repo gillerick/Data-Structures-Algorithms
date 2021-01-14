87c8cc5a98f368673682e4acfbf8aeb224588eec
gillerick <ogayogill95@gmail.com>
2021-01-14 16:56:02 +0300

    Successfully solved

---
+++ Sales by Match.py
@@ -0,0 +1,19 @@
+"""
+Alex works at a clothing store. There is a large pile of socks that must be paired by color for sale.
+Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+For example, there are  socks with colors . There is one pair of color  and one of color .
+There are three odd socks left, one of each color. The number of pairs is .
+
+"""
+n = int(input())
+temp = input().split(' ')
+graph = {}
+for item in temp:
+    graph[item] = 0
+for item in temp:
+    graph[item] += 1
+count = 0
+for item in graph:
+    count += int(graph[item]/2)
+print(count)
