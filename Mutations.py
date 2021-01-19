ada398df2cec88ebba88475b343c49d14b764440
gillerick <ogayogill95@gmail.com>
2021-01-19 11:34:36 +0300

    Solved Find Substring Problem

---
+++ Mutations.py
@@ -0,0 +1,13 @@
+"""
+We have seen that lists are mutable (they can be changed), and tuples are immutable (they cannot be changed).
+You are given an immutable string, and you want to make changes to it.
+"""
+def mutate_string(string, position, character):
+    return f"{string[:position]}{character}{string[position+1:]}"
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
--- Sales by Match.py
+++ Sales by Match.py
@@ -5,15 +5,20 @@ Given an array of integers representing the color of each sock, determine how ma
 For example, there are  socks with colors . There is one pair of color  and one of color .
 There are three odd socks left, one of each color. The number of pairs is .
 
-"""
+# """
 n = int(input())
-temp = input().split(' ')
+temp = input().split()
 graph = {}
+# Initialize the graph item
 for item in temp:
     graph[item] = 0
+# Update graph for items occurring more than once
 for item in temp:
     graph[item] += 1
 count = 0
 for item in graph:
     count += int(graph[item]/2)
 print(count)
+
+
+
