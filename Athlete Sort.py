7a08c239eb3f201e29aab81eba5eb9de8a9d500c
gillerick <ogayogill95@gmail.com>
2021-02-03 00:01:10 +0300

    Sorted the temporary dictionary

--- Athlete Sort.py
+++ Athlete Sort.py
@@ -1,6 +1,14 @@
 rows, columns = input().split()
 record = []
+temp = {}
 for r in range(0, int(rows)):
     record.append(list(map(int, input().split())))
+k = int(input())
+for i in range(len(record)):
+    temp[i] = record[i][k]
+
+sorted_temp = dict(sorted(temp.items(), key=lambda item: item[1]))
 
 print(record)
+print(temp)
+print(sorted_temp)
