40150d090f83f0232ea670f82225012770119cc5
gillerick <ogayogill95@gmail.com>
2021-02-03 22:11:40 +0300

    Athlete sort problem (solved)

--- Athlete Sort.py
+++ Athlete Sort.py
@@ -1,3 +1,10 @@
+"""
+You are given a spreadsheet that contains a list of N athletes and their details (such as age, height, weight and so on).
+ You are required to sort the data based on the Kth attribute and print the final resulting table.
+ Follow the example given below for better understanding.
+
+"""
+
 from typing import List
 
 rows, columns = input().split()
