c69f1cc0d20b581dad70a49a7bf27a72445058fb
gillerick <ogayogill95@gmail.com>
2021-02-15 23:27:51 +0300

    Second method (solved)

--- ginortS.py
+++ ginortS.py
@@ -9,11 +9,11 @@ All sorted odd digits are ahead of sorted even digits.
 
 import string
 
-data = input()
+data = sorted(input())
 
-lowercase = sorted(list(filter(lambda x: x in string.ascii_lowercase, data)))
-uppercase = sorted(list(filter(lambda x: x in string.ascii_uppercase, data)))
-digit = sorted(list(filter(lambda x: x in string.digits, data)))
+lowercase = list(filter(lambda x: x in string.ascii_lowercase, data))
+uppercase = list(filter(lambda x: x in string.ascii_uppercase, data))
+digit = list(filter(lambda x: x in string.digits, data))
 
 result = lowercase + uppercase + list(filter(lambda x: int(x) % 2 != 0, digit)) + list(filter(lambda x: int(x) % 2 == 0, digit))
 
