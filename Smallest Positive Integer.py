355f8071e43fa341b0c061f9c344002bddd38c9d
gillerick <ogayogill95@gmail.com>
2021-03-02 17:06:31 +0300

    Rewrote solution for Smallest Positive Integer

--- Smallest Positive Integer.py
+++ Smallest Positive Integer.py
@@ -11,11 +11,11 @@ def solution(A):
     m = max(A)
     if m < 1:
         return 1
-
-    A = set(A)
-    B = set(range(1, m + 1))
-    D = B - A
+    B = set(A)
+    C = set(range(1, m+1))
+    D = C - B
     if len(D) == 0:
-        return m + 1
+        return m+1
     else:
         return min(D)
+
