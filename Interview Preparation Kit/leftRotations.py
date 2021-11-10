cb6171ff1b4a69aa57de9e7785ccb9c8ee1ea761
Gill Erick <erick@sendyit.com>
2021-11-10 22:53:37 +0300

    Solve left rotation problem

---
+++ Interview Preparation Kit/leftRotations.py
@@ -0,0 +1,17 @@
+def left_rotate(a, d):
+    len_arr = len(a)
+
+    if d > len_arr:
+        d -= len_arr
+
+    first = list(map(str, a[d:]))
+    last = list(map(str, a[:d]))
+    return ' '.join(first + last)
+
+
+if __name__ == "__main__":
+    first_multiple_input = input().split()
+    len_array = int(first_multiple_input[0])
+    rotations = int(first_multiple_input[1])
+    a = list(map(int, input().split()))[:len_array]
+    print(left_rotate(a, rotations))
